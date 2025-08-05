c86feaf9760d565e1d9b88815d6c3ff4c94751d2
Suhai Alvi <alvisuhail3@gmail.com>
2025-08-05 08:21:06 +0530

    middleware & update Profile pic | checkauth

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,15 +1,16 @@
-import express from 'express';
-import { login, logout, signup } from '../controller/auth.controller.js';
+import express from "express";
+import { checkAuth, login, logout, signup, updateProfile } from "../controller/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
+router.put("/update-profile", protectRoute, updateProfile);
 
-router.post("/signup",signup)
+router.get("/check", protectRoute, checkAuth);
 
-router.post("/login",login)
-
-router.post("/logout",logout)
 
 export default router;
-
