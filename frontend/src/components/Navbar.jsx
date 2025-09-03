9e571fccd3a5a6705f82bd77080bb5664dd034fc
Suhai Alvi <alvisuhail3@gmail.com>
2025-09-03 21:39:41 +0530

    Start front end part

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='text-5xl text-amber-100'>Navbar</div>
+    <div className='text-5xl text-amber-100'>Navbar.com</div>
   )
 }
 
